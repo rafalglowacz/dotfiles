845217f108fca2282d5e6601f0c50f6a12d23e11
Rafał Głowacz <rafalglowacz@gmail.com>
2023-04-10 13:31:51 +0200

    add NeoVim config

---
+++ nvim-custom/chadrc.lua
@@ -0,0 +1,4 @@
+local M = {}
+M.ui = {theme = 'oceanic-light'}
+M.plugins = 'custom.plugins'
+return M
---
+++ nvim-custom/plugins.lua
@@ -0,0 +1,19 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      view = {
+        side = "right"
+      }
+    }
+  }
+}
+
+return plugins
