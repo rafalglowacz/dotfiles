12f5646e7ec9a0230f1d312519af17f7061195ec
Rafał Głowacz <rafalglowacz@gmail.com>
2023-04-20 20:30:43 +0200

    (Vim Laracasts): add basic keymaps

---
+++ nvim-custom-laracasts/lua/user/keymaps.lua
@@ -0,0 +1,29 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+vim.keymap.set('v', 'y', 'myy`y')
+
+--vim.keymap.set('n', 'q:', ':q')
+
+vim.keymap.set('v', 'p', '"_dP')
+
+vim.keymap.set('i', ';;', '<Esc>A;');
+vim.keymap.set('i', ',,', '<Esc>A,');
+
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>');
+
+vim.keymap.set('n', '<Leader>x', ':!xdg-open %<CR><CR>')
+
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
+
