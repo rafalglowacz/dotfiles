f790ebb06b670ef28c0f9b012dba98e41637c4d5
Rafał Głowacz <rafalglowacz@gmail.com>
2023-04-21 21:26:32 +0200

    (Vim Laracasts): add more plugins

--- nvim-custom-laracasts/lua/user/plugins.lua
+++ nvim-custom-laracasts/lua/user/plugins.lua
@@ -26,9 +26,71 @@ local use = require('packer').use
 use('wbthomason/packer.nvim')
 
 use('tpope/vim-commentary')
+use('tpope/vim-surround')
+use('tpope/vim-eunuch')
+use('tpope/vim-unimpaired')
+use('tpope/vim-sleuth')
+use('tpope/vim-repeat')
+use('sheerun/vim-polyglot')
+use('christoomey/vim-tmux-navigator')
+use('farmergreg/vim-lastplace')
+use('nelstrom/vim-visual-star-search')
+use('jessarcher/vim-heritage')
+
+use({
+  'whatyouhide/vim-textobj-xmlattr',
+  requires = 'kana/vim-textobj-user',
+})
+
+use({
+  'airblade/vim-rooter',
+  setup = function()
+    vim.g.rooter_manual_only = 1
+  end,
+  config = function()
+    vim.cmd('Rooter')
+  end,
+})
+
+-- use({
+--   'windwp/nvim-autopairs',
+--   config = function()
+--     require('nvim-autopairs').setup()
+--   end,
+-- })
+
+use({
+  'karb94/neoscroll.nvim',
+  config = function()
+    require('neoscroll').setup()
+  end,
+})
+
+use({
+  'famiu/bufdelete.nvim',
+  config = function()
+    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+  end,
+})
+
+use({
+  'AndrewRadev/splitjoin.vim',
+  config = function()
+    vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+    vim.g.splitjoin_trailing_comma = 1
+    vim.g.splitjoin_php_method_chain_full = 1
+  end,
+})
+
+use({
+  'sickill/vim-pasta',
+  config = function()
+    vim.g.pasta_disable_file_types = { 'fugitive' }
+  end,
+})
 
 if packer_bootstrap then
-    require('packer').sync()
+  require('packer').sync()
 end
 
 vim.cmd([[
