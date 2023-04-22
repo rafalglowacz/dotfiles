23a0286240d6b4ef67bd7cc1affa9df9e099864a
Rafał Głowacz <rafalglowacz@gmail.com>
2023-04-22 09:39:59 +0200

    (Vim): disable Rooter

--- nvim-custom-laracasts/lua/user/plugins.lua
+++ nvim-custom-laracasts/lua/user/plugins.lua
@@ -42,15 +42,15 @@ use({
   requires = 'kana/vim-textobj-user',
 })
 
-use({
-  'airblade/vim-rooter',
-  setup = function()
-    vim.g.rooter_manual_only = 1
-  end,
-  config = function()
-    vim.cmd('Rooter')
-  end,
-})
+-- use({
+--   'airblade/vim-rooter',
+--   setup = function()
+--     vim.g.rooter_manual_only = 1
+--   end,
+--   config = function()
+--     vim.cmd('Rooter')
+--   end,
+-- })
 
 use({
   'windwp/nvim-autopairs',
