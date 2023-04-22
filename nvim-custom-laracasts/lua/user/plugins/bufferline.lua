be244e2a4400b3efecbb8bc18ee022af5c01dd2f
Rafał Głowacz <rafalglowacz@gmail.com>
2023-04-22 17:47:48 +0200

    (Vim Laracasts): change Vim icon color in the bufferline

--- nvim-custom-laracasts/lua/user/plugins/bufferline.lua
+++ nvim-custom-laracasts/lua/user/plugins/bufferline.lua
@@ -19,7 +19,7 @@ require('bufferline').setup({
     custom_areas = {
       left = function()
         return {
-          { text = '    ', fg = '#8fff6d' },
+          { text = '    ', fg = '#ffffff' },
         }
       end,
     },
