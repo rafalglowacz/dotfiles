d76fff2c5602c3277cfcb4275dd773168295a9d7
Rafał Głowacz <rafalglowacz@gmail.com>
2023-04-22 20:40:50 +0200

    (Vim Laracasts): add Gitsigns plugin

--- nvim-custom-laracasts/lua/user/plugins.lua
+++ nvim-custom-laracasts/lua/user/plugins.lua
@@ -133,6 +133,19 @@ use({
   end,
 })
 
+use({
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+    vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+    vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+    vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+    vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+    vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+    vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+  end
+})
+
 if packer_bootstrap then
   require('packer').sync()
 end
