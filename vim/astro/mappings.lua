856cdc9890c03a7d963fef7d5a1127bbd5ec3741
Rafał Głowacz <rafalglowacz@gmail.com>
2023-05-01 12:33:44 +0200

    (Vim Astro): add buffer switch mappings using Tab key

---
+++ vim/astro/mappings.lua
@@ -0,0 +1,13 @@
+return {
+  n = {
+    ['<Tab>'] = { 
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, 
+      desc = "Next buffer" 
+    },
+    ['<S-Tab>'] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    }
+  }
+}
+
