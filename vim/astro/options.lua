6b29f6059e1a3e38492af3986ef2dcabd0669e56
Rafał Głowacz <rafalglowacz@gmail.com>
2023-05-19 23:33:16 +0200

    (Vim): add color column, disable relative numbering, set custom wild mode, enable wrap by default

--- vim/astro/options.lua
+++ vim/astro/options.lua
@@ -3,6 +3,10 @@ return {
     resession_enabled = true,
   },
   opt = {
+    colorcolumn = '80,120',
+    relativenumber = false,
     scrolloff = 15,
+    wildmode = 'longest:full,full',
+    wrap = true,
   }
 }
