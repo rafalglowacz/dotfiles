981475156bc5d18998db34150db2a8894b1ff9f5
Rafał Głowacz <rafalglowacz@gmail.com>
2023-06-03 22:36:19 +0200

    (Vim): dim inactive windows

--- vim/astro/plugins/core.lua
+++ vim/astro/plugins/core.lua
@@ -49,6 +49,12 @@ return {
       require('catppuccin').setup({ 
         flavour = 'latte',
         transparent_background = true,
+        dim_inactive = {
+          enabled = true,
+          shade = dark,
+          percentage = 0.15,
+        },
+        -- no_italic = true,
       })
     end
   },
