c98ce2b82c6449b710f459bc216c840bc25da0cd
Rafał Głowacz <rafalglowacz@gmail.com>
2023-06-17 00:01:46 +0200

    (Kitty, Vim): enable cursor blinking

--- vim/astro/options.lua
+++ vim/astro/options.lua
@@ -4,6 +4,7 @@ return {
   },
   opt = {
     colorcolumn = '80,120',
+    guicursor = 'n-v-c-sm:block,i-ci-ve:hor20-Cursor/lCursor,r-cr-o:hor20-Cursor/lCursor,a:blinkon100',
     relativenumber = false,
     scrolloff = 15,
     wildmode = 'longest:full,full',
