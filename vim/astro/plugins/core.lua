31ae3eda631fc41586718f3d1617fc7cc03c52ae
Rafał Głowacz <rafalglowacz@gmail.com>
2023-06-18 17:31:57 +0200

    (Vim): remove transparency from Everforest theme

--- vim/astro/plugins/core.lua
+++ vim/astro/plugins/core.lua
@@ -67,7 +67,7 @@ return {
   {
     'neanias/everforest-nvim',
     config = function()
-      require('everforest').setup({ transparent_background_level = 2 });
+      require('everforest').setup({ transparent_background_level = 0 });
     end
   },
   -- Disabled plugins
