313d7688aade9e01ed34d05ea3af6d2f7eec08ff
Rafał Głowacz <rafalglowacz@gmail.com>
2023-12-02 10:02:03 +0100

    (Wezterm): add local config example for light theme usage

--- wezterm/local.example.lua
+++ wezterm/local.example.lua
@@ -5,8 +5,9 @@ local module = {}
 function module.localConfig(config)
     config.color_scheme = 'Cyberdyne'
     config.window_decorations = 'RESIZE'
-    local theme = require 'baitong'
-    theme.configure(config)
+    --local theme = require 'baitong' -- dark theme
+    --theme.configure(config) -- dark theme
+    config.color_scheme = 'Atelier Lakeside Light (base16)' -- light theme
 end
 
 return module
