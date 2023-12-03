62d4c2440e606ece0c10b8de80b1362fb6b76ba7
Rafał Głowacz <rafalglowacz@gmail.com>
2023-12-03 14:37:28 +0100

    (Wezterm): add inactive_pane_hsb example for light themes

--- wezterm/local.example.lua
+++ wezterm/local.example.lua
@@ -8,6 +8,8 @@ function module.localConfig(config)
     --local theme = require 'baitong' -- dark theme
     --theme.configure(config) -- dark theme
     config.color_scheme = 'Atelier Lakeside Light (base16)' -- light theme
+    -- For light themes different desaturation and dimming works better.
+    config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.8 } -- light theme
 end
 
 return module
