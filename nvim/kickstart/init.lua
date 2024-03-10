c9271721b9d7f91bad39b2e0de961a7dde737fb6
Rafał Głowacz <rafalglowacz@gmail.com>
2024-03-10 20:43:15 +0100

    (Neovim): add autocmd to restore cursor to last position

--- nvim/kickstart/init.lua
+++ nvim/kickstart/init.lua
@@ -210,6 +210,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Return cursor to where it was last time closing the file',
+  group = vim.api.nvim_create_augroup('restore-cursor-position', { clear = true }),
+  pattern = '*',
+  command = 'silent! normal! g`"zv',
+})
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
