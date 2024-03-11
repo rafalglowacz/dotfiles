27fa0d0e02dcef2943acc78eb68e7492e592d6b1
Rafał Głowacz <rafalglowacz@gmail.com>
2024-03-11 21:01:58 +0100

    (Neovim): disable format on save
    It breaks .vue files and it'd cause other issues.

--- nvim/kickstart/init.lua
+++ nvim/kickstart/init.lua
@@ -647,10 +647,6 @@ require('lazy').setup({
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
