12f5c72fb582569d479fefc369e079cdc34cb4c3
Rafał Głowacz <rafalglowacz@gmail.com>
2024-04-06 22:51:47 +0200

    (Neovim): add undo history

--- nvim/kickstart/init.lua
+++ nvim/kickstart/init.lua
@@ -336,6 +336,22 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'debugloop/telescope-undo.nvim',
+    keys = { { '<leader>u', '<Cmd>Telescope undo<CR>', desc = '[U]ndo history' } },
+    opts = {
+      extensions = {
+        undo = {
+          diff_context_lines = 3,
+          side_by_side = true,
+        },
+      },
+    },
+    config = function(_, opts)
+      require('telescope').setup(opts)
+      require('telescope').load_extension('undo')
+    end,
+  },
 
   { 'mfussenegger/nvim-dap', config = function()
       local dap = require'dap'
