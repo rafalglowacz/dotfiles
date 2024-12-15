3be354c6be43e3e1af757f2ebcee21d6a8333343
Rafał Głowacz <rafalglowacz@gmail.com>
2024-12-15 16:02:20 +0100

    (Neovim): add more PHP snippets

--- nvim/lua/user/snippets/php.lua
+++ nvim/lua/user/snippets/php.lua
@@ -3,11 +3,40 @@ local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 
 ls.add_snippets('php', {
-  ls.snippet(
-    'inf',
-    fmt(
-      'info("[rafal-tmp] {}");',
-      { i(0) }
-    )
-  ),
+    ls.snippet(
+        'fn',
+        fmt(
+            'function {}() {{\n'..
+                '    {}\n'..
+                '}}',
+            { i(1), i(0) }
+        )
+    ),
+    ls.snippet(
+        'inf',
+        fmt(
+            'info("[rafal-tmp] {}");',
+            { i(0) }
+        )
+    ),
+    ls.snippet(
+        'pf',
+        fmt(
+            'public function {}(): {}\n'..
+                '{{\n'..
+                '    {}\n'..
+                '}}',
+            { i(1), i(2, 'void'), i(0) }
+        )
+    ),
+    ls.snippet(
+        'pr',
+        fmt(
+            'private function {}(): {}\n'..
+                '{{\n'..
+                '    {}\n'..
+                '}}',
+            { i(1), i(2, 'void'), i(0) }
+        )
+    ),
 })
