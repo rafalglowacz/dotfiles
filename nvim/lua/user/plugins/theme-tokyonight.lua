5284febb47c7832fdb1de6e525da91545f527c9e
Rafał Głowacz <rafalglowacz@gmail.com>
2024-12-18 20:18:47 +0100

    (Neovim): make color columns and some highlights less pronounced

--- nvim/lua/user/plugins/theme-tokyonight.lua
+++ nvim/lua/user/plugins/theme-tokyonight.lua
@@ -5,11 +5,17 @@ return {
     config = function()
         require('tokyonight').setup{
             transparent = true,
+            on_highlights = function (hl)
+                if vim.o.background == 'light' then
+                    print('on_highlights')
+                    hl.ColorColumn.bg = '#eeeeee'
+                    hl.CursorLine.bg = '#eeeeee'
+                    hl.LspReferenceRead.bg = '#f1f1f1'
+                    hl.LspReferenceWrite.bg = '#f1f1f1'
+                end
+            end
         }
         -- Try also 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
         vim.cmd.colorscheme 'tokyonight-night'
-
-        -- Highlights can be configured by doing something like
-        vim.cmd.hi 'Comment gui=none'
     end,
 }
