d298743d7411353a027a4c34562d4d12df330768
Rafał Głowacz <rafalglowacz@gmail.com>
2024-12-20 23:03:04 +0100

    (Neovim): make custom nvim-tree bindings specified consistently with builtin ones, open sidebar by default

--- nvim/lua/user/plugins/sidebar.lua
+++ nvim/lua/user/plugins/sidebar.lua
@@ -8,12 +8,21 @@ return {
                 width = 35,
             },
             on_attach = function(bufNr)
+                local function opts(desc)
+                    return {
+                        desc = "nvim-tree: " .. desc, buffer = bufNr, noremap = true, silent = true, nowait = true
+                    }
+                end
+
                 api.config.mappings.default_on_attach(bufNr)
-                vim.keymap.set('n', '<right>', api.node.open.preview, { buffer = bufNr })
-                vim.keymap.set('n', '<left>', api.node.navigate.parent_close, { buffer = bufNr })
+                vim.keymap.set('n', '<right>', api.node.open.preview, opts('Open'))
+                vim.keymap.set('n', '<left>', api.node.navigate.parent_close, opts('Close'))
             end
         })
         vim.keymap.set('n', '<A-0>', '<Cmd>:NvimTreeFocus<CR>')
         vim.keymap.set('n', '<leader>F', '<Cmd>:NvimTreeFindFile<CR>')
+        if vim.fn.expand('%:t') ~= 'COMMIT_EDITMSG' then
+            vim.cmd('NvimTreeFocus')
+        end
     end
 }
