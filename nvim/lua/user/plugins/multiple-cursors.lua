58a0fb3d768d87bc2198daebb2d7dbca40271cd6
Rafał Głowacz <rafalglowacz@gmail.com>
2024-12-22 14:46:33 +0100

    (Neovim): add smoka7/multicursors.nvim
    I might have to switch to something else because cursors in the insert mode are in the wrong place* but it's quite impressive nevertheless, so I'm keeping the changes to have a working configuration for it.

---
+++ nvim/lua/user/plugins/multiple-cursors.lua
@@ -0,0 +1,57 @@
+return {
+    'smoka7/multicursors.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        'nvimtools/hydra.nvim',
+    },
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+        {
+            mode = { 'v', 'n' },
+            '<Leader>m',
+            '<cmd>MCunderCursor<cr>',
+            desc = 'Start multicursors',
+        },
+        {
+            mode = { 'v', 'n' },
+            '<Leader>M',
+            '<cmd>MCstart<cr>',
+            desc = 'Start multicursors for selection',
+        },
+    },
+    config = function ()
+        local N = require('multicursors.normal_mode')
+        local E = require('multicursors.extend_mode')
+
+        require('multicursors').setup({
+            normal_keys = {
+                ['<up>'] = {
+                    method = N.create_up,
+                    opts = {},
+                },
+                ['<down>'] = {
+                    method = N.create_down,
+                    opts = {},
+                },
+            },
+            extend_keys = {
+                ['<up>'] = {
+                    method = E.k_method,
+                    opts = { desc = 'char up' },
+                },
+                ['<left>'] = {
+                    method = E.h_method,
+                    opts = { desc = 'char left' },
+                },
+                ['<down>'] = {
+                    method = E.j_method,
+                    opts = { desc = 'char down' },
+                },
+                ['<right>'] = {
+                    method = E.l_method,
+                    opts = { desc = 'char right' },
+                },
+            },
+        })
+    end
+}
