d842cdfbdd9a3193c32b849907bb5bfcb93ba03c
Rafał Głowacz <rafalglowacz@gmail.com>
2024-12-25 11:05:56 +0100

    (Neovim): add PHP to list of languages for Treesitter

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -37,7 +37,7 @@ return {
     build = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+            ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'php', 'vim', 'vimdoc' },
             auto_install = true,
             ignore_install = { 'gitcommit' },
             highlight = { enable = true },
