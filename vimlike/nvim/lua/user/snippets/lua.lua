903a6955a035fedb649b65a40fc6062eb034e74e
Rafał Głowacz <rafalglowacz@gmail.com>
2024-12-28 00:38:24 +0100

    (Neovim): add user log Lua snippet

--- vimlike/nvim/lua/user/snippets/lua.lua
+++ vimlike/nvim/lua/user/snippets/lua.lua
@@ -3,11 +3,18 @@ local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 
 ls.add_snippets('lua', {
-  ls.snippet(
-    'fn',
-    fmt(
-      'function()\n    {}\nend',
-      { i(0) }
-    )
-  ),
+    ls.snippet(
+        'fn',
+        fmt(
+            'function()\n    {}\nend',
+            { i(0) }
+        )
+    ),
+    ls.snippet(
+        'ul',
+        fmt(
+            "require('user.log').info({})",
+            { i(0) }
+        )
+    ),
 })
