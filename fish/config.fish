127a958a12924565aa6f9345853f6b66d9ac9a3e
Rafał Głowacz <rafalglowacz@gmail.com>
2025-01-06 10:54:20 +0100

    (Fish): refactor PATH manipulation

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,15 @@
 if status is-interactive
-    fish_add_path ~/.composer/vendor/bin
-    fish_add_path ~/.config/composer/vendor/bin
-    fish_add_path ~/bin
-    fish_add_path ~/.cargo/bin
-    fish_add_path ~/.local/bin
-    fish_add_path ~/.local/bin/go/bin
-    # if [ -d "$HOME/bin2" ]; then
-    #     MY_PATH="$MY_PATH:$HOME/bin2"
-    # fi
-    # if [ -d "$HOME/dev/flutter" ]; then
-    #     MY_PATH="$MY_PATH:$HOME/dev/flutter/bin"
-    # fi
-    # if [ -d /opt/homebrew ]; then
-    #     MY_PATH="$MY_PATH:/opt/homebrew/opt/mysql-client/bin:/opt/homebrew/opt/ruby/bin"
-    # fi
-    # if [ -d "$HOME/.local/share/nvim/mason/bin" ]; then
-    #     MY_PATH="$MY_PATH:$HOME/.local/share/nvim/mason/bin"
-    # fi
-    # export PATH="$MY_PATH:$PATH"
+    fish_add_path -P ~/.cargo/bin
+    fish_add_path -P ~/.composer/vendor/bin
+    fish_add_path -P ~/.config/composer/vendor/bin
+    fish_add_path -P ~/.local/bin
+    fish_add_path -P ~/.local/bin/go/bin
+    fish_add_path -P ~/.local/share/nvim/mason/bin
+    fish_add_path -P ~/bin
+    fish_add_path -P ~/bin2
+    fish_add_path -P ~/dev/flutter/bin
+    fish_add_path -P /opt/homebrew/opt/mysql-client/bin
+    fish_add_path -P /opt/homebrew/opt/ruby/bin
 
     alias ls='eza --icons --group-directories-first -a --hyperlink'
     alias lh='ls -lh --time-style relative'
