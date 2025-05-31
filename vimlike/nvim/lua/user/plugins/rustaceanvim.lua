b3b662abcd5b84f3578cdee22d0c4462cdab9eee
Rafał Głowacz <rafalglowacz@gmail.com>
2025-05-31 22:25:56 +0200

    (Neovim): update packages

--- vimlike/nvim/lua/user/plugins/rustaceanvim.lua
+++ vimlike/nvim/lua/user/plugins/rustaceanvim.lua
@@ -1,6 +1,6 @@
 return {
     'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
+    version = '^6',
     lazy = false, -- This plugin is already lazy
     keys = {
         -- For <leader>dc see dap.lua.
