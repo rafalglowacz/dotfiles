6740483d4a92501419a1e71dc7defe619d32ce2d
Rafał Głowacz <rafalglowacz@gmail.com>
2025-08-17 12:19:56 +0200

    (Fish): fix duplicated spaces

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt --description 'Informative prompt'
     set -g __fish_git_prompt_showdirtystate yes
     set -g __fish_git_prompt_showcolorhints yes
 
-    #Save the return status of the previous command
+    # Save the return status of the previous command
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
@@ -20,16 +20,15 @@ function fish_prompt --description 'Informative prompt'
         echo -n (set_color brblack)''
         echo -n (set_color white -b brblack; set_color --bold)(date "+%H:%M:%S")' │ '
         echo -n (set_color normal)(set_color white -b brblack)(prompt_pwd --full-length-dirs 99)
-        echo -n (set_color normal)(set_color brblack)''(set_color normal)' '
+        echo -n (set_color normal)(set_color brblack)''(set_color normal)
 
-        # Jobs count pill (yellow) shown only when there are background jobs
+        # Jobs count pill (yellow) shown only when there are background jobs.
         set -l jobc (count (jobs -p))
         if test $jobc -gt 0
-            echo -n (set_color yellow)''(set_color black -b yellow)$jobc(set_color normal)(set_color yellow)''(set_color normal)' '
+            echo -n ' '(set_color yellow)''(set_color black -b yellow)$jobc(set_color normal)(set_color yellow)''(set_color normal)
         end
 
-        # Pipe status, git info
-        echo -n $pipestatus_string' '
+        echo -n $pipestatus_string
         echo -n (fish_git_prompt)
         echo
         echo -n (set_color blue)'➤'(set_color normal)'  '
