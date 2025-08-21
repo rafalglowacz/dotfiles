d93c5bcfee395bf033693fc2971d15e7bab9dbf6
Rafał Głowacz <rafalglowacz@gmail.com>
2025-08-21 21:23:17 +0200

    (Fish): move error code to right prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,20 +3,12 @@ function fish_prompt --description 'Informative prompt'
     set -g __fish_git_prompt_showdirtystate yes
     set -g __fish_git_prompt_showcolorhints yes
 
-    # Save the return status of the previous command
-    set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-
     if functions -q fish_is_root_user; and fish_is_root_user
         printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
                                                          and set_color $fish_color_cwd_root
                                                          or set_color $fish_color_cwd) \
             (prompt_pwd) (set_color normal)
     else
-        set -l status_color (set_color $fish_color_status)
-        set -l statusb_color (set_color black -b $fish_color_status --bold)
-        set -l pipestatus_string (__fish_print_pipestatus " " "" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
         echo -n (set_color brblack)''
         echo -n (set_color white -b brblack; set_color --bold)(date "+%H:%M:%S")' │ '
         echo -n (set_color normal)(set_color white -b brblack)(prompt_pwd --full-length-dirs 99)
@@ -28,7 +20,6 @@ function fish_prompt --description 'Informative prompt'
             echo -n ' '(set_color yellow)''(set_color black -b yellow)$jobc(set_color normal)(set_color yellow)''(set_color normal)
         end
 
-        echo -n $pipestatus_string
         echo -n (fish_git_prompt)
         echo
         echo -n (set_color blue)'➤'(set_color normal)'  '
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,7 +1,17 @@
 function fish_right_prompt
-    if test $CMD_DURATION != "0"
+    # Save the return status of the previous command
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color black -b $fish_color_status --bold)
+    set -l pipestatus_string (__fish_print_pipestatus " " "" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+    echo -n $pipestatus_string
+
+    if test $CMD_DURATION != 0
         # Show duration of the last command in seconds
         set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.2fs", $1 / $2}')
-        echo $duration
+        echo " $duration"
     end
 end
