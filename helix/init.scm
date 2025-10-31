885493d5d2fc14cf24aeaef2d67d6c45132603aa
Rafał Głowacz <rafalglowacz@gmail.com>
2025-10-31 17:09:11 +0100

    (Helix): add formatter for Scheme

--- helix/init.scm
+++ helix/init.scm
@@ -1,4 +1,6 @@
 (require "helix/configuration.scm")
 (define-lsp "steel-language-server" (command "steel-language-server") (args '()))
 (define-language "scheme"
+                 (formatter (command "raco") (args '("fmt" "-i")))
+                 (auto-format #true)
                  (language-servers '("steel-language-server")))
