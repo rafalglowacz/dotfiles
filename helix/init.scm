00f17bb9686a0e3944298d291b2ac24779fcf549
Rafał Głowacz <rafalglowacz@gmail.com>
2025-10-31 10:22:37 +0100

    (Helix): add base helix.scm and init.scm files

---
+++ helix/init.scm
@@ -0,0 +1,4 @@
+(require "helix/configuration.scm")
+(define-lsp "steel-language-server" (command "steel-language-server") (args '()))
+(define-language "scheme"
+                 (language-servers '("steel-language-server")))
