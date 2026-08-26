23c48a78ad421c7957e6440b0659ec398d88b653
Rafał Głowacz <rafalglowacz@gmail.com>
2026-08-26 15:23:39 +0200

    (Fish): change theme to Ayu

--- fish/conf.d/fish_frozen_theme.fish
+++ fish/conf.d/fish_frozen_theme.fish
@@ -5,42 +5,33 @@
 #     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
 # for appropriate commands to add to ~/.config/fish/config.fish instead.
 
-set --global fish_color_autosuggestion 8e908c
+set --global fish_color_autosuggestion 8A9199
 set --global fish_color_cancel --reverse
-set --global fish_color_command 8959a8
-set --global fish_color_comment eab700
-set --global fish_color_cwd green
+set --global fish_color_command 55B4D4
+set --global fish_color_comment ABB0B6
+set --global fish_color_cwd 399EE6
 set --global fish_color_cwd_root red
-set --global fish_color_end 8959a8
-set --global fish_color_error c82829
-set --global fish_color_escape 00a6b2
-set --global fish_color_gray 
+set --global fish_color_end ED9366
+set --global fish_color_error F51818
+set --global fish_color_escape 4CBF99
 set --global fish_color_history_current --bold
 set --global fish_color_host --reset
 set --global fish_color_host_remote yellow
-set --global fish_color_keyword 
-set --global fish_color_match --background=brblue
-set --global fish_color_normal --reset
-set --global fish_color_operator 00a6b2
-set --global fish_color_option 
-set --global fish_color_param 4271ae
-set --global fish_color_quote 718c00
-set --global fish_color_redirection 3e999f
-set --global fish_color_search_match white --bold --background=brblack
-set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_normal 575F66
+set --global fish_color_operator FF9940
+set --global fish_color_param 575F66
+set --global fish_color_quote 86B300
+set --global fish_color_redirection A37ACC
+set --global fish_color_search_match --bold --background=FF9940
+set --global fish_color_selection --bold --background=FF9940
 set --global fish_color_status red
 set --global fish_color_user brgreen
 set --global fish_color_valid_path --underline=single
-set --global fish_pager_color_background 
 set --global fish_pager_color_completion --reset
 set --global fish_pager_color_description B3A06D
 set --global fish_pager_color_prefix --bold --underline=single
 set --global fish_pager_color_progress brwhite --bold --background=cyan
-set --global fish_pager_color_secondary_background 
-set --global fish_pager_color_secondary_completion 
-set --global fish_pager_color_secondary_description 
-set --global fish_pager_color_secondary_prefix 
-set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_background --background=FF9940
 set --global fish_pager_color_selected_completion 
 set --global fish_pager_color_selected_description 
 set --global fish_pager_color_selected_prefix 
