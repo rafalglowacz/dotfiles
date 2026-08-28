0370475dfb63eef22c023feeb84d21ab2e2283ad
Rafał Głowacz <rafalglowacz@gmail.com>
2026-08-28 08:59:46 +0200

    (Fish): tweak theme - better green for command

--- fish/conf.d/fish_frozen_theme.fish
+++ fish/conf.d/fish_frozen_theme.fish
@@ -7,21 +7,21 @@
 
 set --global fish_color_autosuggestion 64DF85
 set --global fish_color_cancel --reverse
-set --global fish_color_command 00BF32
+set --global fish_color_command green
 set --global fish_color_comment 5C9900
 set --global fish_color_cwd green
 set --global fish_color_cwd_root red
 set --global fish_color_end 8EEB00
 set --global fish_color_error 60B9CE
 set --global fish_color_escape 00a6b2
-set --global fish_color_gray 
+set --global fish_color_gray
 set --global fish_color_history_current --bold
 set --global fish_color_host --reset
 set --global fish_color_host_remote yellow
-set --global fish_color_keyword 
+set --global fish_color_keyword
 set --global fish_color_normal --reset
 set --global fish_color_operator 00a6b2
-set --global fish_color_option 
+set --global fish_color_option
 set --global fish_color_param 04819E
 set --global fish_color_quote 206676
 set --global fish_color_redirection 7CB02C
@@ -30,16 +30,16 @@ set --global fish_color_selection white --bold --background=brblack
 set --global fish_color_status red
 set --global fish_color_user brgreen
 set --global fish_color_valid_path --underline=single
-set --global fish_pager_color_background 
+set --global fish_pager_color_background
 set --global fish_pager_color_completion --reset
 set --global fish_pager_color_description B3A06D
 set --global fish_pager_color_prefix --bold --underline=single
 set --global fish_pager_color_progress brwhite --bold --background=cyan
-set --global fish_pager_color_secondary_background 
-set --global fish_pager_color_secondary_completion 
-set --global fish_pager_color_secondary_description 
-set --global fish_pager_color_secondary_prefix 
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
 set --global fish_pager_color_selected_background --background=brblack
-set --global fish_pager_color_selected_completion 
-set --global fish_pager_color_selected_description 
-set --global fish_pager_color_selected_prefix 
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
